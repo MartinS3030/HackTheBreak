d394fc937d9f8f3140749865473c4c9d1090bad9
MADDELIN MADDELIN <maddelin.tania@gmail.com>
2023-03-11 11:07:54 -0800

    display html

--- snake_game.py
+++ snake_game.py
@@ -1,12 +1,8 @@
 from flask import Flask
+from views import views
 
 app = Flask(__name__)
-
-
-@app.route("/")
-def home():
-    return "this  is the home page"
-
+app.register_blueprint(views, url_prefix='/views')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
