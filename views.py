f2db260b17238a2b7a1a0e460802140599bff6ae
MADDELIN MADDELIN <maddelin.tania@gmail.com>
2023-03-11 11:45:14 -0800

    complete tutorial

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
 
 views = Blueprint(__name__, 'views')
 
@@ -10,6 +10,17 @@ def home():
 
 @views.route('/profile')
 def profile():
-    args = request.args
-    name = args.get('name')
-    return render_template('index.html', name=name)
+    return render_template('profile.html')
+
+@views.route("/json")
+def get_json():
+    return jsonify({"name": "Joe", "age": 20})
+
+@views.route("/data")
+def get_data():
+    data = request.json
+    return jsonify(data)
+
+@views.route("/go-to-home")
+def go_to_home():
+    return redirect(url_for('views.home'))
